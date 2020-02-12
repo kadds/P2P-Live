6927ce8409f7d56886b011d30f070fc3c074c8d3
kadds <itmyxyf@gmail.com>
2020-02-12 19:30:54 +0800

    add rudp file

---
+++ include/net/rudp.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "event.hpp"
+#include "udp.hpp"
+
+namespace net::rudp
+{
+
+class rudp_server_t
+{
+};
+
+class rudp_client_t
+{
+};
+
+}; // namespace net::rudp
---
+++ lib/net/rudp.cc
@@ -0,0 +1,6 @@
+#include "net/rudp.hpp"
+#include "net/socket.hpp"
+namespace net::rudp
+{
+
+} // namespace net::rudp
