edf93e7bbd9f8eefc66e0056e767af26ad4f6c8c
YShaw <442789171@qq.com>
2020-02-13 14:08:38 +0800

    update

--- src/source-client/main-window.cc
+++ src/source-client/main-window.cc
@@ -7,13 +7,20 @@
 #define PIXEL_HEIGHT 180
 #define BPP 12
 
+#define MAX_AUDIO_FRAME_SIZE 192000
+
 int codec_id_Video, codec_id_Audio;     // SDL播放辨别的Video格式
 int index_Video = -1, index_Audio = -1; //解码时对应的视频音频标记（video是1，audio是0
 int width_Video, height_Video;
+SDL_AudioSpec sdl_audios ;
 SDL_Window *sdl_screen = NULL;
 SDL_Renderer *sdl_renderer = NULL;
 SDL_Texture *sdl_texture = NULL;
 SDL_Rect sdl_rect;
+
+Uint8 *MainWindow::audio_chunk=0;
+Uint32 MainWindow::audio_len=0;
+Uint8 *MainWindow::audio_pos=0;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -22,11 +29,12 @@ MainWindow::MainWindow(QWidget *parent)
     // ui->setupUi(this);
     // this->setWindowTitle("P2P Live");
     show_dshow_device();
-    demux("test.flv", "outvideo", "outaudio");
+    demux("test2.mp4", "outvideo2.yuv", "outaudio2.pcm");
     // SDL();
     // RGBToYUV(320,568);
     std::cout << "程序正常运行" << std::endl;
 }
+
 
 void MainWindow::show_dshow_device()
 {
@@ -81,13 +89,16 @@ int MainWindow::RGBToYUV(int width, int height)
 }
 inline double MainWindow::r2d(AVRational r) { return r.num == 0 || r.den == 0 ? 0.0 : (double)r.num / (double)r.den; }
 //解码
-int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAudioFile)
+int MainWindow::demux(char *pInputFile, char *pOutputFileName_Video, char *pOutputFileName_Audio)
 {
     AVFormatContext *pAVFC = avformat_alloc_context();
-    AVCodecContext *pAVCC;
+    AVCodecContext *pAVCC_Video;
+    AVCodecContext *pAVCC_Audio;
     AVPacket *pAVP = av_packet_alloc();
-    AVFrame *pAVF = av_frame_alloc();
-    AVFrame *pAVF_YUV = av_frame_alloc();
+    AVFrame *pAVF_Audio = av_frame_alloc();
+    AVFrame *pAVF_Video = av_frame_alloc();
+    AVFrame *pAVF_Video_YUV = av_frame_alloc();
+    int fps=0;
 
     // av_register_all();//旧版本函数已弃用
     // avcodec_register_all();//旧版本函数已弃用
@@ -105,25 +116,40 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
     for (int i = 0; i < pAVFC->nb_streams; i++)
     {
         AVStream *pAVS = pAVFC->streams[i];
+        AVCodec *pAVC = avcodec_find_decoder(pAVS->codecpar->codec_id);//查找当前流的编码器
+        if (pAVC == NULL)
+            ErrorExit("查找编码器失败");
+        else cout<<"success codec";
 
         if (pAVS->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
         {
-            string format, codec_id; //音频采样格式,压缩编码格式
-
-            /*if (AV_CODEC_ID_AAC == pAVS->codecpar->codec_id){
-                codec_id_Audio=AV_CODEC_ID_AAC;
-                codec_id = "AAC";
+            string format; //音频采样格式,压缩编码格式
+            ///编码器
+            pAVCC_Audio = avcodec_alloc_context3(pAVC);
+            //pAVCC_Video = pAVFC->streams[i]->codec;
+            re = avcodec_parameters_to_context(pAVCC_Audio, pAVS->codecpar);
+            if (!pAVCC_Audio)
+                ErrorExit("分配编码器失败");
+            int re = avcodec_open2(pAVCC_Audio, pAVC, nullptr);
+            if (re != 0)
+            {
+                avcodec_free_context(&pAVCC_Audio);
+                ErrorExit("打开编码器失败");
             }
-            else if (AV_CODEC_ID_MP3 == pAVS->codecpar->codec_id){
-                codec_id_Audio=AV_CODEC_ID_MP3;
-                codec_id = "MP3";
-            }*/
+            else
+                cout << "打开编码器成功" << endl;
 
-            if (pAVS->codecpar->format == AV_SAMPLE_FMT_FLTP)
-                format = "AV_SAMPLE_FMT_FLTP";
+
+
+            ///打印内容赋值
+            if (pAVS->codecpar->format == AV_SAMPLE_FMT_S16)
+                format = "AV_SAMPLE_FMT_S16";
             else if (pAVS->codecpar->format == AV_SAMPLE_FMT_S16P)
                 format = "AV_SAMPLE_FMT_S16P";
-
+            else if(pAVS->codecpar->format == AV_SAMPLE_FMT_FLT)
+                format = "AV_SAMPLE_FMT_FLT";
+            else if (pAVS->codecpar->format == AV_SAMPLE_FMT_FLTP)
+                format = "AV_SAMPLE_FMT_FLTP";
             index_Audio = pAVS->index;
             int durationAudio = (pAVS->duration) * r2d(pAVS->time_base);
             int s = durationAudio % 60;
@@ -131,22 +157,33 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
             int h = durationAudio / 3600;
 
             //信息打印
-            cout << "音频信息:" << endl;
-            cout << "音频采样率:" << pAVS->codecpar->sample_rate << "Hz" << endl;
+            cout << "========音频流信息========" << endl;
+            cout << "音频采样率:" << pAVCC_Audio->sample_rate << "Hz" << endl;
             cout << "音频信道数目:" << pAVS->codecpar->channels << endl;
             cout << "音频采样格式:" << format << endl;
-            // cout << "音频压缩编码格式:" << codec_id << endl;
+            cout << "音频压缩编码格式:" << pAVC->name << endl;
             cout << "音频总时长：" << h << "时" << m << "分" << s << "秒" << endl;
         }
         else if (pAVS->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) //如果是视频流，则打印视频的信息
         {
-            /*string codec_id; //压缩编码格式
-            if (pAVS->codecpar->codec_id==AV_CODEC_ID_MPEG4  ){
-                codec_id_Video=AV_CODEC_ID_MPEG4;
-                codec_id = "MPEG4";
-            }*/
+            ///分配视频编码器
+            pAVCC_Video = avcodec_alloc_context3(pAVC);
+            //pAVCC_Video = pAVFC->streams[i]->codec;
+            re = avcodec_parameters_to_context(pAVCC_Video, pAVS->codecpar);
+            if (!pAVCC_Video)
+                ErrorExit("分配编码器失败");
+            int re = avcodec_open2(pAVCC_Video, pAVC, nullptr);
+            if (re != 0)
+            {
+                avcodec_free_context(&pAVCC_Video);
+                ErrorExit("打开编码器失败");
+            }
+            else
+                cout << "打开编码器成功" << endl;
 
+            ///打印内容赋值
             index_Video = pAVS->index;
+            fps= r2d(pAVS->avg_frame_rate);
             int durationVideo = (pAVS->duration) * r2d(pAVS->time_base);
             int s = durationVideo % 60;
             int m = (durationVideo % 3600) / 60;
@@ -154,78 +191,110 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
             height_Video = pAVS->codecpar->height;
             width_Video = pAVS->codecpar->width;
 
-            cout << "视频信息:" << endl;
-            cout << "视频帧率:" << r2d(pAVS->avg_frame_rate) << "fps" << endl; //表示每秒出现多少帧
+            cout << "========视频流信息========" << endl;
+            cout << "视频帧率:" << fps << "fps" << endl; //表示每秒出现多少帧
             cout << "帧高度:" << pAVS->codecpar->height << endl;
             cout << "帧宽度:" << pAVS->codecpar->width << endl;
+            cout << "视频压缩编码格式:" << pAVC->name << endl;
             cout << "视频总时长:" << h << "时" << m << "分" << s << "秒" << endl;
+
         }
 
-        AVCodec *pAVC = avcodec_find_decoder(pAVS->codecpar->codec_id);
-        if (pAVC == NULL)
-            ErrorExit("查找编码器失败");
-        else
-            std::cout << "压缩编码格式:" << pAVC->name << std::endl;
-
-        if (AVMEDIA_TYPE_VIDEO == pAVS->codecpar->codec_type)
-        {
-
-            pAVCC = avcodec_alloc_context3(pAVC);
-            // pAVCC = pAVFC->streams[i]->codec;
-
-            re = avcodec_parameters_to_context(pAVCC, pAVS->codecpar);
-            if (!pAVCC)
-                ErrorExit("分配编码器失败");
             /**
              * 编码内容
-            pAVCC->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; //便于输出获取
-            pAVCC->codec_id = pAVC->id;
-            pAVCC->thread_count = 2; //线程数量
+            pAVCC_Video->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; //便于输出获取
+            pAVCC_Video->codec_id = pAVC->id;
+            pAVCC_Video->thread_count = 2; //线程数量
 
             //仅视频：
-            pAVCC->bit_rate = 200 * 1024 * 8; //压缩后每秒视频的比特位大小：200kB
-            pAVCC->width = WIDTH;
-            pAVCC->height = HEIGHT;
-            pAVCC->time_base = {1, 25}; // 1,fps
-            pAVCC->framerate = {25, 1}; // fps,1
+            pAVCC_Video->bit_rate = 200 * 1024 * 8; //压缩后每秒视频的比特位大小：200kB
+            pAVCC_Video->width = WIDTH;
+            pAVCC_Video->height = HEIGHT;
+            pAVCC_Video->time_base = {1, 25}; // 1,fps
+            pAVCC_Video->framerate = {25, 1}; // fps,1
 
             //画面组大小，多少帧一个关键帧
-            pAVCC->gop_size = 50;
-            pAVCC->max_b_frames = 0; //最大b帧
-            pAVCC->pix_fmt = AV_PIX_FMT_YUV420P;*/
+            pAVCC_Video->gop_size = 50;
+            pAVCC_Video->max_b_frames = 0; //最大b帧
+            pAVCC_Video->pix_fmt = AV_PIX_FMT_YUV420P;*/
 
-            int re = avcodec_open2(pAVCC, pAVC, nullptr);
-            if (re != 0)
-            {
-                avcodec_free_context(&pAVCC);
-                ErrorExit("打开编码器失败");
-            }
-            else
-                cout << "打开编码器成功" << endl;
-        }
     }
 
     av_init_packet(pAVP);
-    cout << pAVCC->width << " " << pAVCC->height;
-
-    SwsContext *pSC = NULL;
-    pSC = sws_getCachedContext(pSC, pAVCC->width, pAVCC->height, pAVCC->pix_fmt, //源
-                               pAVCC->width, pAVCC->height, AV_PIX_FMT_YUV420P,  //目标
+    /**
+     * @brief pSC_Video
+     */
+    SwsContext *pSC_Video = NULL;
+    pSC_Video = sws_getCachedContext(pSC_Video, pAVCC_Video->width, pAVCC_Video->height, pAVCC_Video->pix_fmt, //源
+                               pAVCC_Video->width, pAVCC_Video->height, AV_PIX_FMT_YUV420P,  //目标
                                SWS_BICUBIC,                                      //尺寸变化算法
                                0, 0, 0);
-    uint8_t *out_buffer = (uint8_t *)av_malloc(avpicture_get_size(AV_PIX_FMT_YUV420P, pAVCC->width, pAVCC->height));
-    avpicture_fill((AVPicture *)pAVF_YUV, out_buffer, AV_PIX_FMT_YUV420P, pAVCC->width, pAVCC->height);
-    // av_image_fill_arrays();
-
-    // FILE *out=fopen("out_test.yuv","wb");
     /**
-     * SDL
-     *
+     * @brief pSC_Audio
+     */
+    cout<<"---开始分配音频上下文---"<<endl;
+    SwrContext *pSC_Audio=swr_alloc();
+    //分配参数
+    AVSampleFormat AVSF_out=AV_SAMPLE_FMT_S16;
+    AVSampleFormat AVSF_in=pAVCC_Audio->sample_fmt;
+    int inSampleRate=pAVCC_Audio->sample_rate;
+    int outSampleRate=pAVCC_Audio->sample_rate;
+    int outSamplesNumber=pAVCC_Audio->frame_size;//假定输出=输入一帧数据量
+    int inChannelLayout=av_get_default_channel_layout(pAVCC_Audio->channels);
+
+    uint64_t outChannelLayout=AV_CH_FRONT_LEFT;//输出通道数
+    int outChannelLayoutNumber=av_get_channel_layout_nb_channels(outChannelLayout);
+    cout<<outChannelLayout<<"---"<<inChannelLayout<<"---"<<outChannelLayout<<endl;
+    pSC_Audio=swr_alloc_set_opts(pSC_Audio,
+                                 1,AVSF_out,outSampleRate,
+                                 1,AVSF_in,inSampleRate,
+                                 0,NULL);
+
+    swr_init(pSC_Audio);
+
+
+    cout<<"---音频上下文over---"<<endl;
+    /**
+     * @brief outBuffer_Video
+     * @brief outBuffer_Audio
+     * 输入输出文件打开
      */
 
-    if (SDL_Init(SDL_INIT_VIDEO)) //成功，非0
+
+    cout<<"---打开输入输出文件---"<<endl;
+    FILE *pOutputFile_Audio=fopen(pOutputFileName_Audio,"wb");
+    FILE *pOutputFile_Video=fopen(pOutputFileName_Video,"wb");
+    if(!pOutputFile_Audio||!pOutputFile_Video)ErrorExit("输入输出文件打开失败");
+
+    int outBufferSize_Video=avpicture_get_size(AV_PIX_FMT_YUV420P, pAVCC_Video->width, pAVCC_Video->height);
+    uint8_t *outBuffer_Video = (uint8_t *)av_malloc(outBufferSize_Video);
+    avpicture_fill((AVPicture *)pAVF_Video_YUV, outBuffer_Video, AV_PIX_FMT_YUV420P, pAVCC_Video->width, pAVCC_Video->height);
+
+    int outBufferSize_Audio=av_samples_get_buffer_size(NULL,outChannelLayoutNumber,outSamplesNumber,AVSF_out, 1);
+    uint8_t *outBuffer_Audio=(uint8_t *)av_malloc(MAX_AUDIO_FRAME_SIZE*2);
+    cout<<"---打开输入输出over---"<<endl;
+
+    /**
+     * SDL INIT
+     * SDL_AudioSpec
+     */
+    cout<<"---sdl init---"<<endl;
+    void (*pcallback)(void*,Uint8*,int) = &SDLAudio;
+
+    sdl_audios.freq=outSampleRate;//采样率
+    sdl_audios.format=AUDIO_S16SYS;
+    sdl_audios.channels=outChannelLayout;
+    sdl_audios.silence=0;//静音
+    sdl_audios.samples=outSamplesNumber;
+    sdl_audios.callback=pcallback;
+    sdl_audios.userdata = pAVCC_Audio;
+
+    if (SDL_OpenAudio(&sdl_audios, NULL)!=0)
+        ErrorExit("can't open audio.\n");
+
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_TIMER)) //成功，非0
         ErrorExit("SDL init error");
-    sdl_screen = SDL_CreateWindow("new sdl window", 0, 0, pAVCC->width, pAVCC->height, SDL_WINDOW_RESIZABLE);
+    sdl_screen = SDL_CreateWindow("new sdl window", 0, 0, pAVCC_Video->width, pAVCC_Video->height, SDL_WINDOW_RESIZABLE);
     if (sdl_screen == NULL)
         ErrorExit("SDL window error");
 
@@ -233,10 +302,12 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
     if (!sdl_renderer)
         ErrorExit("SDL render error");
 
-    sdl_texture =
-        SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_TARGET, pAVCC->width, pAVCC->height);
+    sdl_texture = SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_TARGET, pAVCC_Video->width, pAVCC_Video->height);
     if (!sdl_texture)
         ErrorExit("SDL sdl_texture");
+
+    cout<<SDL_GetError();
+    SDL_PauseAudio(0);
 
     while (av_read_frame(pAVFC, pAVP) >= 0)
     {
@@ -244,27 +315,39 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
         if (pAVP->stream_index == index_Video)
         {
             // int frameFinished=0;
-            // int len=avcodec_decode_video2(pAVCC, pAVF, &frameFinished, pAVP);弃用
-            re = avcodec_send_packet(pAVCC, pAVP);
-            cout << "send:" << re << " ";
-
+            // int len=avcodec_decode_video2(pAVCC_Video, pAVF_Video, &frameFinished, pAVP);弃用
+            re = avcodec_send_packet(pAVCC_Video, pAVP);//re==0:success
             av_packet_unref(pAVP);
-
-            re = avcodec_receive_frame(pAVCC, pAVF);
-            cout << "receive:" << re << " " << endl;
+            re = avcodec_receive_frame(pAVCC_Video, pAVF_Video);//re==0:success
             if (re == 0)
             {
 
-                sws_scale(pSC, pAVF->data, pAVF->linesize, 0, pAVCC->height, pAVF_YUV->data, pAVF_YUV->linesize);
-                /*fwrite(pAVF_YUV->data[0],1,pAVCC->width*pAVCC->height,out);//输出Y数据
-                fwrite(pAVF_YUV->data[1],1,pAVCC->width*pAVCC->height/4,out);//输出U数据
-                fwrite(pAVF_YUV->data[2],1,pAVCC->width*pAVCC->height/4,out);//输出V数据*/
-                SDL(pAVF_YUV->data[0], pAVF_YUV->data[1], pAVF_YUV->data[2], pAVCC->width, pAVCC->height, 25);
+                sws_scale(pSC_Video, pAVF_Video->data, pAVF_Video->linesize, 0, pAVCC_Video->height, pAVF_Video_YUV->data, pAVF_Video_YUV->linesize);
+                fwrite(pAVF_Video_YUV->data[0],1,pAVCC_Video->width*pAVCC_Video->height,pOutputFile_Video);//输出Y数据
+                fwrite(pAVF_Video_YUV->data[1],1,pAVCC_Video->width*pAVCC_Video->height/4,pOutputFile_Video);//输出U数据
+                fwrite(pAVF_Video_YUV->data[2],1,pAVCC_Video->width*pAVCC_Video->height/4,pOutputFile_Video);//输出V数据
+                SDL(pAVF_Video_YUV->data[0], pAVF_Video_YUV->data[1], pAVF_Video_YUV->data[2], pAVCC_Video->width, pAVCC_Video->height,fps);
+
             }
         }
         else if (pAVP->stream_index == index_Audio)
         {
-            cout << "~~~" << endl;
+            re = avcodec_send_packet(pAVCC_Audio, pAVP);//re==0:success
+            av_packet_unref(pAVP);
+            re = avcodec_receive_frame(pAVCC_Audio, pAVF_Audio);//re==0:success
+
+            re=swr_convert(pSC_Audio,&outBuffer_Audio, MAX_AUDIO_FRAME_SIZE,(const uint8_t **)pAVF_Audio->data , pAVF_Audio->nb_samples);
+            outBufferSize_Audio = re * outChannelLayoutNumber * av_get_bytes_per_sample(AVSF_out);
+
+            fwrite(outBuffer_Audio,1,outBufferSize_Audio,pOutputFile_Audio);
+            while(audio_len>0)//Wait until finish
+                     SDL_Delay(1);
+            //Set audio buffer (PCM data)
+            audio_chunk = (Uint8*) outBuffer_Audio;
+            //Audio buffer length
+            audio_len = outBufferSize_Audio;
+            audio_pos = audio_chunk;
+cout<<re;
         }
         else
             cout << "其他帧";
@@ -279,27 +362,7 @@ int MainWindow::demux(char *pInputFile, char *pOutputVideoFile, char *pOutputAud
 int MainWindow::SDL(uint8_t *YPlane, uint8_t *UPlane, uint8_t *VPlane, int width, int height, int fps)
 {
     //文件直接播放改为ffmpeg解码播放
-    // FILE *f = fopen("352_288.yuv", "rb+");
-    // if (f == NULL)  ErrorExit( "file open error");
-    // if(codec_id_Video)
 
-    // unsigned char buffer[PIXEL_HEIGHT * PIXEL_WIDTH * BPP / 8];
-
-    /*unsigned char *YPlane = (unsigned char *)malloc(width * height);
-    unsigned char *UPlane = (unsigned char *)malloc(width * height / 4);
-    unsigned char *VPlane = (unsigned char *)malloc(width * height / 4);*/
-
-    /*
-    while (1)
-    {
-        int sizeY = fread(YPlane, 1, WIDTH * HEIGHT, f);
-        int sizeU = fread(UPlane, 1, WIDTH * HEIGHT / 4, f);
-        int sizeV = fread(VPlane, 1, WIDTH * HEIGHT / 4, f);
-        if (!sizeY || !sizeU || !sizeV)
-            break;
-
-        std::cout << sizeY << " " << sizeU << " " << sizeV << std::endl;
-*/
     int re = SDL_UpdateYUVTexture(sdl_texture, NULL, YPlane, width, UPlane, width / 2, VPlane, width / 2);
     if (re)
         std::cout << "SDL update error: " << SDL_GetError() << std::endl;
@@ -312,9 +375,6 @@ int MainWindow::SDL(uint8_t *YPlane, uint8_t *UPlane, uint8_t *VPlane, int width
     SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, &sdl_rect);
     SDL_RenderPresent(sdl_renderer);
     SDL_Delay(fps);
-    /*}
-    fclose(f);*/
-
     return 0;
 }
 
@@ -333,6 +393,7 @@ int MainWindow::InInit(char *in_url, AVFormatContext *&in_avfc)
     av_dump_format(in_avfc, 0, in_url, 0);
     return 0;
 }
+
 
 int MainWindow::OutInit(char *out_url, AVFormatContext *&out_avfc)
 {
--- src/source-client/main-window.hpp
+++ src/source-client/main-window.hpp
@@ -9,7 +9,9 @@ extern "C" {
 #include<libavutil/avutil.h>
 #include<libavutil/time.h>
 #include<libswscale/swscale.h>
+#include<libswresample/swresample.h>
 #include<SDL2/SDL.h>
+#include<SDL2/SDL_audio.h>
 }
 using namespace std;
 
@@ -36,9 +38,39 @@ class MainWindow : public QMainWindow
     inline double r2d(AVRational r);
     int demux(char* pInputFile,char* pOutputVideoFile,char* pOutputAudioFile);
     int SDL(uint8_t *YPlane,uint8_t *UPlane,uint8_t *VPlane, int width, int height,int fps);
-    
+
+
+
+    static Uint8 *audio_chunk;
+    static Uint32 audio_len;
+    static Uint8 *audio_pos;
+    static void SDLAudio(void *udata,Uint8 *stream,int len){
+        SDL_memset(stream, 0, len);
+        /*while (len > 0)
+            {
+                if (audio_len == 0)
+                    continue;
+                int temp = (len > audio_len ? audio_len : len);
+                SDL_MixAudio(stream, audio_pos, temp, SDL_MIX_MAXVOLUME);
+                audio_pos += temp;
+                audio_len -= temp;
+                stream += temp;
+                len -= temp;
+            }*/
+        if(audio_len==0)
+                return;
+
+            len=(len>audio_len?audio_len:len);	/*  Mix  as  much  data  as  possible  */
+
+            SDL_MixAudio(stream,audio_pos,len,SDL_MIX_MAXVOLUME);
+            audio_pos += len;
+            audio_len -= len;
+    }
+
     ~MainWindow();
 
   private:
     Ui::MainWindow *ui;
 };
+
+
